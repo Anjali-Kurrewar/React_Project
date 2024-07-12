856e1941f896f2bdbfe54cc530839815a4703a72
Anjali Kurrewar <anjali.kurrewar1234@gmail.com>
2024-07-12 02:18:54 -0700

    first commit

---
+++ Project01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import background from './components/background'
+
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <background />
+    </div>
+  )
+}
+
+export default App
